989f5bec101a0c194579666f49fbd460057cdc7d
Saju <vijayasajanuki@gmail.com>
2023-12-19 00:39:19 -0330

    Initial Code Commit

---
+++ js/math/primitives/point.js
@@ -0,0 +1,20 @@
+class Point {
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    draw(ctx, size = 18, color = "black") {
+
+        const rad = size / 2;
+        ctx.beginPath();
+        ctx.fillStyle = color;
+        //arc method of context has following attributes
+        //start location x and y
+        //radius
+        //start and end of the circle in radians(important)
+        ctx.arc(this.x, this.y, rad, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
